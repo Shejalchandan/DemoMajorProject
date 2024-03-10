82ea4d7b66c4fb5695781cb29cfa553f331cbe6c
Shejal Chandan <138048164+Shejalchandan@users.noreply.github.com>
2024-03-10 18:06:59 +0530

    Add files via upload

---
+++ ecommerce-project/src/main/java/com/ecommerce/config/jwtConstant.java
@@ -0,0 +1,6 @@
+package com.ecommerce.config;
+
+public class jwtConstant {
+	public static final String SECRET_KEY="hdksdsnmmnkjijisoidjasijd ksjdlasjdasj";
+	public static final String JWT_HEADER="Authorization";
+}
